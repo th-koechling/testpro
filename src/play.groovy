e0a79a91897088aad18efc353940f3a6323118d1
th-koechling <thorsten.koechling@bioinfsys.uni-giessen.de>
2016-12-02 13:58:25 +0100

    add stuff

--- src/play.groovy
+++ src/play.groovy
@@ -6,3 +6,17 @@
 def array = [2, 4, 1, 6, 8, 2, 1, 9]
 
 array.each { print "${it} blah " }
+String hi = "hi!!"
+String out = sprintf('\n%1$s\n', hi)
+
+def fact (num) {
+    if (num <= 1) {
+        return num
+    }
+    else {
+        return num * fact(num - 1)
+    }
+}
+
+println out
+println fact(4)
